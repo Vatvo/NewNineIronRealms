70b8d7d6e99ce310e8417e677d1b6268848d1789
Vatvo <34244664+Vatvo@users.noreply.github.com>
2026-01-14 14:43:11 -0500

    Shot Aiming

---
+++ Entities/Player/AimMarker.gd
@@ -0,0 +1,62 @@
+extends Node3D
+
+class_name AimMarker
+
+@onready var ball: RigidBody3D = $".."
+
+func draw_aim(shotForce: Vector3, aimLength) -> void:
+	for child in get_children():
+		child.queue_free()
+	
+	var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity", 9.8)
+	var timeStep := 0.05
+	
+	var velocity: Vector3 = Vector3(shotForce.x, shotForce.y, shotForce.z).normalized() * 30
+	
+	var initPosition: Vector3 = ball.global_position
+	
+	var linearDamp: float = ball.linear_damp + ProjectSettings.get_setting("physics/3d/default_linear_damp", 0)
+	
+	var markerStart: Vector3 = initPosition
+	var markerEnd: Vector3 = initPosition
+	
+	for i in range(1, aimLength + 1):
+		markerEnd = markerStart
+		markerEnd += velocity * timeStep
+		
+		velocity *= clampf(1.0 - linearDamp * timeStep, 0, 1)
+		
+		var point = MeshInstance3D.new()
+		var size = 0.5
+		point.scale = Vector3(size, size, size)
+		point.mesh = SphereMesh.new()
+		point.position = markerEnd
+		#point.material_override = load("res://Materials/ShotAimMarker.tres")
+		add_child(point)
+			
+		var ray := raycast_query(markerStart, markerEnd + markerEnd.normalized())
+		
+		if ray.has("collider") && ray["collider"] != ball:
+			velocity = velocity.bounce(ray.normal) * ball.physics_material_override.bounce
+			
+			var leftoverTime: float = markerStart.distance_squared_to(ray.position) / markerStart.distance_squared_to(markerEnd)
+			leftoverTime *= timeStep
+			
+			var leftoverSpeed: Vector3 = velocity
+			leftoverSpeed.y -= gravity * leftoverTime
+			leftoverSpeed *= (1.0 + linearDamp * leftoverTime)
+			
+			markerStart = ray.position
+			markerStart += leftoverSpeed * leftoverTime
+			
+			continue
+		
+		markerStart = markerEnd
+		
+func raycast_query(start: Vector3, end: Vector3) -> Dictionary:
+	var space = get_viewport().get_world_3d().direct_space_state
+	var rayQuery = PhysicsRayQueryParameters3D.new()
+	rayQuery.from = start
+	rayQuery.to = end
+	rayQuery.set_collision_mask(1)
+	return space.intersect_ray(rayQuery)
--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -3,31 +3,100 @@ extends RigidBody3D
 class_name Player
 
 static var canMoveCamera: bool = true
+static var canShoot: bool = true
 
-@onready var camera: PhantomCamera3D = $Camera
-
+@onready var cameraHost: PhantomCamera3D = $CameraHost
+@onready var shotPullLine: Line2D = $ShotUI/ShotPullLine
+@onready var shotUI: CanvasLayer = $ShotUI
+@onready var aimMarker: AimMarker = $AimMarker
+@onready var mainCamera: Camera3D = get_tree().get_nodes_in_group("MainCamera")[0]
 @export_category("Control Parameters")
 @export var cameraSensitivity: Vector2 = Vector2(1,1)
 @export var cameraDistanceCurve: Curve
 
-# Called when the node enters the scene tree for the first time.
+var isShooting: bool = false
+var currentShotPower: float = 0.0
+
 func _ready() -> void:
-	pass # Replace with function body.
+	var newCameraRotation: Vector3 = cameraHost.get_third_person_rotation()
+	newCameraRotation.x = clamp(newCameraRotation.x, -PI/2 + 0.1, -0.5)
+	cameraHost.set_third_person_rotation(newCameraRotation)
+	cameraHost.spring_length = cameraDistanceCurve.sample(cameraHost.get_third_person_rotation().x)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
+	if Input.is_action_just_released("Shoot") && isShooting:
+		shoot(0, 0)
+		
+	if Input.is_action_pressed("Shoot") && canShoot:
+		isShooting = true
+		shotUI.visible = true
+		aimMarker.visible = true
+		
+		var screenSize: Vector2 = get_viewport().size
+		var mousePos: Vector2 = get_viewport().get_mouse_position()
+		var centerScreen: Vector2 = get_viewport().size / 2
+		
+		var centeredMousePos: Vector2 = mousePos - centerScreen
+		var direction: float = atan2(centeredMousePos.y, centeredMousePos.x)
+		
+		var length: float = mousePos.distance_to(centerScreen)
+		length = clamp(length, 0, screenSize.y / 3)
+		
+		var pullLineEnd: Vector2 = update_pull_line(direction, length)
+		var aimDirection = get_aim_direction(pullLineEnd, screenSize)
+		aimMarker.draw_aim(aimDirection, 3)
+	else:
+		isShooting = false
+		shotUI.visible = false
+		aimMarker.visible = false
+		
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_pressed("MoveCamera") && event is InputEventMouseMotion\
 	&& canMoveCamera:
-		var currentCameraRotation := camera.get_third_person_rotation()
+		var currentCameraRotation := cameraHost.get_third_person_rotation()
 		var newCameraRotation := currentCameraRotation
 		newCameraRotation.y -= event.relative.x * cameraSensitivity.x
 		newCameraRotation.x -= event.relative.y * cameraSensitivity.y
-		newCameraRotation.x = clamp(newCameraRotation.x, -PI/2 + 0.1, 0)
-		camera.set_third_person_rotation(newCameraRotation)
+		newCameraRotation.x = clamp(newCameraRotation.x, -PI/2 + 0.1, -0.5)
+		cameraHost.set_third_person_rotation(newCameraRotation)
 		
-		camera.spring_length = cameraDistanceCurve.sample(newCameraRotation.x)
+		cameraHost.spring_length = cameraDistanceCurve.sample(newCameraRotation.x)
+
+func shoot(direction: float, length: float) -> void:
+	print("Shoot")
+
+func raycast_mouse_to_xz_plane(mousePos: Vector2) -> Dictionary:
+	var plane = Plane(Vector3.UP, global_position.y)
+	
+	var from = mainCamera.project_ray_origin(mousePos)
+	var to = mainCamera.project_ray_normal(mousePos)
+	
+	var intersect = plane.intersects_ray(from, to)
+	if intersect:
+		return {"success":true, "value":intersect}
+	else:
+		return {"success":false, "value":Vector3(0,0,0)}
+	
+func update_pull_line(direction: float, length: float) -> Vector2:
+	var screenSize: Vector2 = get_viewport().size
+	shotPullLine.points[0] = screenSize / 2
+	var uncenteredCoords: Vector2 = Vector2(length * cos(direction), length * sin(direction))
+	shotPullLine.points[1] = uncenteredCoords + (screenSize / 2)
+	
+	return uncenteredCoords + (screenSize/2)
+
+func get_aim_direction(pullLineEnd: Vector2, screenSize: Vector2):
+	var planeIntersect = raycast_mouse_to_xz_plane(pullLineEnd)
+	#var oppositePlaneIntersect = raycast_mouse_to_xz_plane(screenSize - pullLineEnd)
+
+	if planeIntersect["success"]: #&& oppositePlaneIntersect["success"]:
+		var intersectPoint: Vector3 = planeIntersect["value"]
+		#var oppositePoint: Vector3 = oppositePlaneIntersect["value"]
 		
+		return intersectPoint.direction_to(self.position)#intersectPoint.direction_to(oppositePoint)
+	else:
+		var screenDirection: Vector2 = (screenSize/2).direction_to(pullLineEnd)
+		screenDirection.x *= -1
+		screenDirection = screenDirection.rotated(mainCamera.rotation.y)
+		
+		return Vector3(screenDirection.x, 0, -screenDirection.y)
