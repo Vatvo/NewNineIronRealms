9c52b04268877b4aa4a1a5ebe2c78cb5d79981c1
Big-Geebs <lukewharless@gmail.com>
2026-01-26 23:01:00 -0500

    Started pause functionality build

--- UserInterface/PauseUI/pause_menu.gd
+++ UserInterface/PauseUI/pause_menu.gd
@@ -2,12 +2,14 @@ extends Control
 
 @onready var Animator: AnimationPlayer = $"../AnimationPlayer"
 
+var canUnpause: bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Animator.play("FadeIn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+		#if Input.is_action_just_released("Pause") && canUnpause == false:
+		await get_tree().create_timer(0.1).timeout
+		canUnpause = true
