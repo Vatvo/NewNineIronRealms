06d85e8751a54b7b19e7975f852f310be3a56f48
Big-Geebs <lukewharless@gmail.com>
2026-01-26 23:41:34 -0500

    Fixed spin animation and fade animation issues

--- UserInterface/PauseUI/pause_menu.gd
+++ UserInterface/PauseUI/pause_menu.gd
@@ -1,15 +1,20 @@
 extends Control
 
-@onready var Animator: AnimationPlayer = $"../AnimationPlayer"
+@onready var FadeAnimator: AnimationPlayer = $"../FadeAnimPlayer"
+@onready var SpinAnimator: AnimationPlayer = $"../SpinAnimPlayer"
 
 var canUnpause: bool = false
 
 func _ready() -> void:
-	Animator.play("FadeIn")
-
+	FadeAnimator.play("FadeIn")
+	SpinAnimator.play("RuneSpin2")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-		#if Input.is_action_just_released("Pause") && canUnpause == false:
+	if Input.is_action_just_released("Pause") && canUnpause == false:
 		await get_tree().create_timer(0.1).timeout
 		canUnpause = true
+
+	if Input.is_action_just_released("Pause") && canUnpause == true:
+		get_tree().paused = false
+		FadeAnimator.play_backwards("FadeIn")
