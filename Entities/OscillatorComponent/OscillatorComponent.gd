f38e970141fc0d498b5fe2c9fb3681231e8d2024
Vatvo <34244664+Vatvo@users.noreply.github.com>
2026-01-28 17:59:39 -0500

    Upgraded Oscillator

--- Entities/OscillatorComponent/OscillatorComponent.gd
+++ Entities/OscillatorComponent/OscillatorComponent.gd
@@ -3,14 +3,14 @@ extends Node
 
 class_name OscillatorComponent
 
-@export var startRot: Vector3
-@export var endRot: Vector3
+@export var startTransform: Transform3D
+@export var endTransform: Transform3D
 @export var speed: float
 @export var curve: Curve
 @export var enabled: bool = true
 
-@export_tool_button("Bake Start Rotation", "RotateRight") var startButton = bake_start_rot
-@export_tool_button("Bake End Rotation", "RotateLeft") var endButton = bake_end_rot
+@export_tool_button("Bake Start Transform", "Transform3D") var startButton = bake_start_rot
+@export_tool_button("Bake End Transform", "Transform3D") var endButton = bake_end_rot
 
 var parent: Node3D 
 var progress: float = 0
@@ -32,12 +32,13 @@ func _process(delta: float) -> void:
 		else:
 			positive = true
 	
-	parent.rotation.x = lerp_angle(startRot.x, endRot.x, curve.sample(progress))
-	parent.rotation.y = lerp_angle(startRot.y, endRot.y, curve.sample(progress))
-	parent.rotation.z = lerp_angle(startRot.z, endRot.z, curve.sample(progress))
+	parent.transform = lerp(startTransform, endTransform, curve.sample(progress))
+	#parent.rotation.x = lerp_angle(startTransform.x, endTransform.x, curve.sample(progress))
+	#parent.rotation.y = lerp_angle(startTransform.y, endTransform.y, curve.sample(progress))
+	#parent.rotation.z = lerp_angle(startTransform.z, endTransform.z, curve.sample(progress))
 			
 func bake_start_rot() -> void:
-	startRot = parent.rotation
+	startTransform = parent.transform
 	
 func bake_end_rot() -> void:
-	endRot = parent.rotation
+	endTransform = parent.transform
