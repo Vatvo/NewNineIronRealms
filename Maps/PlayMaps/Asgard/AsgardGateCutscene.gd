882f06536ba1788e6a67ad8446c676225a9a8189
Vatvo <34244664+Vatvo@users.noreply.github.com>
2026-03-09 18:47:28 -0400

    Door Cutscene

--- Maps/PlayMaps/Asgard/AsgardGateCutscene.gd
+++ Maps/PlayMaps/Asgard/AsgardGateCutscene.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var button: CollisionButton
-@export var door: Node3D
+@export var door: AsgardDoor
 @export var player: Player
 
 @export var camera: PhantomCamera3D
@@ -10,12 +10,14 @@ func _ready() -> void:
 	button.triggered.connect(play_cutscene)
 
 func play_cutscene() -> void:
-	print("PLAY")
+	
 	player.linear_velocity = Vector3.ZERO
 	player.angular_velocity = Vector3.ZERO
 	
 	camera.priority = 300
 	
-	await get_tree().create_timer(8).timeout
+	await get_tree().create_timer(1).timeout
+	door.open()
+	await get_tree().create_timer(5).timeout
 	
 	camera.priority = 0
